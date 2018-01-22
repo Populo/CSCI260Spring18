e2c8bca0f8d03d30870042c346656f389a3b9164
Chris <staudigelc@xavier.edu>
2018-01-22 13:23:47 -0500

    It runs with no errors currently. All set up

---
+++ src/main/java/edu/xavier/csci260/spring2018/GroupProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.xavier.csci260.spring2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupProjectApplication.class, args);
+    }
+}
